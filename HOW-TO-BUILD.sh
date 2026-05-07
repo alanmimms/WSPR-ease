62e4b7f725b0530a9e9a7b0bff7240e5c5258d24
Alan Mimms <alanmimms@gmail.com>
2026-05-07 15:49:13 -0700

    Add more instructions to build

--- HOW-TO-BUILD.sh
+++ HOW-TO-BUILD.sh
@@ -1,2 +1,5 @@
 . SETUP-ENV.bash
 make -C FPGA && west build -p always -b esp32s3_devkitm/esp32s3/procpu sw && tools/wspr-flash.sh
+
+# Then on a machine directly connected to the target:
+# cd sw && . ./.venv/bin/activate && ../tools/flash-lfs.sh && ../tools/monitor-console.sh
