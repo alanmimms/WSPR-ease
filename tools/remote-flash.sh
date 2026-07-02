11259f447ec899464b4e878bd8ad0c75c9f2b1e9
Alan Mimms <alanmimms@gmail.com>
2026-07-01 17:34:17 -0700

    Now using RP2040 as a DDS. I can use SPI co-host to program its (SPI) flash. I can use I2C between ESP32 and RP2040 to send commands and receive status.

--- tools/remote-flash.sh
+++ tools/remote-flash.sh
@@ -14,6 +14,6 @@ REMOTE_VENV_LIB="/homealan/zephyr-projects/.venv/lib/python3.12/site-packages"
 ssh ${TARGET_IP} "PYTHONPATH=${REMOTE_VENV_LIB} python3 -m \
   esptool --chip esp32s3 \
   --baud 921600 \
-  --port /dev/ttyACM0 \
+  --port ${TARGET_PORT} \
   --baud 921600 \
   write-flash 0x0 ${TARGET_BIN}"
